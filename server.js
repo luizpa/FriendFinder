8a8096f9c46872cecc24be2e6057ae45579d0d5e
Coding <coding@Paulo.attlocal.net>
2018-09-29 23:15:49 -0400

    assignment

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
